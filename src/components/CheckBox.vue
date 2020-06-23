7e3d461c8e667e6005a35a73824b3bb050b72efe
tarzan37 <xuannhat.it@gmail.com>
2020-06-23 23:21:55 +0700

    first commit

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="contrainer">
+        <input type="checkbox" >
+        <label>{{ msg }}</label>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CheckBox',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <CheckBox msg="male"/>
+  </div>
+</template>
+
+<script>
+
+import CheckBox from '@/components/CheckBox.vue'
+
+export default {
+  name: 'About',
+  components: {
+    CheckBox
+  }
+}
+</script>
+
+<style>
+
+</style>
